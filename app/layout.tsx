2ea75a7c403bc9c4ba570ed1f89166456032999f
congreo <miniballl@naver.com>
2026-02-20 11:57:37 +0900

    동의

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '동서대학교 2025학년도 전기학위수여식',
+  title: '동의대학교 2025학년도 전기학위수여식',
   description: '가장 빛나는 오늘의 당신을 위해 특별한 포토부스를 준비했습니다.',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 }
